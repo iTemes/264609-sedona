6e566a7f0913b70bd26e0654dcd9c794b4423598
iTemes <akstemes@gmai.com>
2016-11-13 17:14:53 +0300

    Оформление контента+js
    контент+js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var btn = document.querySelector(".btn-modal");
+var form = document.querySelector(".hotel-search");
+form.classList.add("hidden");
+btn.addEventListener("click",function(event){
+  event.preventDefault();
+  console.log("Click!")
+  form.classList.toggle("show")
+})
